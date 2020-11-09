513ebd14f6429b9a0ee36bff90465e84db13c79e
Ashishchaursiya <chaurasiyaashish383@gmail.com>
2020-11-09 14:11:39 +0530

    Add all my files

--- expense_project/settings.py
+++ expense_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 from django.contrib import messages
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'pv$jrk%n9)cs#29))89w*cjue$s(g=9gmedbisyk(q9bpg9cu)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -145,4 +146,4 @@ EMAIL_PORT = 587
 EMAIL_HOST_PASSWORD =os.environ.get('EMAIL_HOST_PASSWORD')
 
 
- 
+django_heroku.settings(locals())
